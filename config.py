fb4305062ce47a9232693bc207b682f94edac436
Francisco Chen <chenfrancisco435@gmail.com>
2026-03-18 21:15:25 -0400

    Update config.py

--- config.py
+++ config.py
@@ -3,14 +3,14 @@ vocab_size = 32000
 max_seq_len = 128
 d_model = 256
 n_heads = 4
-n_layers = 4
+n_layers = 6
 d_ff = 1024
 dropout = 0.1
 
-batch_size = 16
+batch_size = 8
 lr = 3e-4
 weight_decay = 0.01
-epochs = 3
+epochs = 10
 warmup_steps = 1000
 grad_accum_steps = 1
 device = "cuda"
