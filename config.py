348197df2d2d5f7181f3bcb3cf7819a28ac0fe8b
Francisco Chen <chenfrancisco435@gmail.com>
2026-03-18 22:20:39 -0400

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ d_model = 256  # Transformer hidden size / embedding dimension
 n_heads = 4  # Size of attention heads in multi-head self-attention
 n_layers = 6  # Size of Transformer blocks
 d_ff = 1024  # Hidden dimension of the FFN
-dropout = 0.1  # Dropout probability applied during training
+dropout = 0.15  # Dropout probability applied during training
 
 batch_size = 8  # Training batch size
 lr = 1e-4  # Learning rate for the optimizer.
 weight_decay = 0.01  # weight decay
-epochs = 10  # Number of full passes over the training dataset
+epochs = 5  # Number of full passes over the training dataset
 warmup_steps = 1000  # Warmup steps for the learning-rate schedule
 grad_accum_steps = 1  # Gradient accumulation steps to simulate a larger batch
 device = "cuda"  # GPU
